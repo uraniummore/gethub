8230c32e343c1f70abf13c9e32d2ddadc754d8cf
Sulik <uraniummore@gmail.com>
2024-03-08 17:16:31 +0600

    Created maintenance tasks and entries.

--- backend/vehicle_management/models.py
+++ backend/vehicle_management/models.py
@@ -5,8 +5,8 @@ class Car(models.Model):
     title = models.CharField(max_length=100)
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    year = models.IntegerField()
-    mileage = models.IntegerField()
+    year = models.PositiveIntegerField()
+    mileage = models.PositiveIntegerField()
     vin = models.CharField(max_length=17, unique=True)
 
     def __repr__(self):
@@ -14,3 +14,17 @@ class Car(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class MaintenanceTask(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    mileage_threshold = models.PositiveIntegerField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+
+class MaintenanceEntry(models.Model):
+    maintenance_date = models.DateField()
+    elapsed_mileage = models.PositiveIntegerField()
+    notes = models.TextField(max_length=1000)
+    task = models.ForeignKey(MaintenanceTask, on_delete=models.CASCADE)
